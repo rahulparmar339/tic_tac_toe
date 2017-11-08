3c5988b3c63781ccc80fdfa74e56969267cf4fd3
rahulparmar339 <rahul.parmar.58173000>
2017-11-08 17:40:46 +0530

    indexDB implemented

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
+    
+    var intialgameState = JSON.parse(localStorage.getItem('gameState'));
+    if(intialgameState.turn === undefined){
+      intialgameState.turn = 'X';
+      intialgameState.turnLocked = false;
+      intialgameState.boardArray = Array(9).fill('');
+      intialgameState.count = 0;
+      intialgameState.gameEnded = false;
+    }
+    var intialResult = JSON.parse(localStorage.getItem('result'));
+    if(intialResult === undefined ){
+      intialResult = '';
+    }
+
     this.state = {
-      result : '' 
+      result : intialResult
     }
     this.gameState = {
-      turn : 'X' , 
-      turnLocked : false ,
-      boardArray : Array(9).fill('') ,
-      count : 0 ,
-      gameEnded : false 
+      turn : intialgameState.turn , 
+      turnLocked : intialgameState.turnLocked ,
+      boardArray : intialgameState.boardArray ,
+      count : intialgameState.count ,
+      gameEnded : intialgameState.gameEnded 
     }
+
+    console.log(JSON.parse(localStorage.getItem('gameState')));
+    console.log(this.gameState);
   }
 
   handleClick(square){
@@ -21,31 +38,36 @@ class App extends Component {
 
     var squareId = square.id;
     if(this.gameState.boardArray[squareId] === ''){
-      
+      console.log(this.gameState.turn);
+
       square.innerText = this.gameState.turn;
       this.gameState.boardArray[squareId] = this.gameState.turn;
       this.gameState.count = this.gameState.count + 1 ;
+      this.gameState.turn = (this.gameState.turn === 'X') ? 'O' : 'X';
 
-      var result = this.checkWinner();
-      if(result === 'X'){
+      var winner = this.checkWinner();
+      var result = '';
+      if(winner === 'X'){
         this.gameState.gameEnded = true;
+        result = 'Congratulation You won the game' ;
         this.setState({
-          result : 'Congratulation You won the game'  
+          result : result  
         });
-      }else if(result === 'O'){
+      }else if(winner === 'O'){
         this.gameState.gameEnded = true;
+        result = 'Sorry!! computer won the game'
         this.setState({
-          result : 'Sorry!! computer won the game' 
+          result : result  
         });
-      }else if(result === 'draw'){
+      }else if(winner === 'draw'){
         this.gameState.gameEnded = true;
+        result = 'Ohhh!! Game is draw' ;
         this.setState ({
-          result : 'Ohhh!! Game is draw'
+          result : result
         });
       }
-      console.log(this.gameState.turn);
+      this.updateLocalStorage(result);
 
-      this.gameState.turn = (this.gameState.turn === 'X') ? 'O' : 'X';
 
       if(this.gameState.turn === 'O' && !this.gameState.gameEnded) {
         this.computerCall();
@@ -54,6 +76,34 @@ class App extends Component {
     }
   }  
   
+  updateLocalStorage(result){
+    localStorage.setItem('gameState',JSON.stringify(this.gameState));  
+    localStorage.setItem('result',JSON.stringify(result));        
+  }
+
+  restartGame(){
+    var intialgameState = {
+      turn : 'X' , 
+      turnLocked : false,
+      boardArray : Array(9).fill('') ,
+      count : 0 ,
+      gameEnded : false
+    }
+    localStorage.setItem('gameState',JSON.stringify(intialgameState));
+    localStorage.setItem('result',JSON.stringify(''));
+
+    this.gameState = intialgameState;
+    console.log(this.gameState);
+    console.log("game restarted");
+    
+    for(var i=0;i<9;i++){
+      document.querySelectorAll('.square')[i].innerText = this.gameState.boardArray[i] ;  
+    }
+    this.setState({
+      result : 'Game is restarted'
+    });
+  }
+
   computerCall(){
     this.gameState.turnLocked = true;
     setTimeout(()=> {
@@ -62,7 +112,7 @@ class App extends Component {
       } while(this.gameState.boardArray[random] !== '');
       this.gameState.turnLocked = false;
       this.handleClick(document.querySelectorAll('.square')[random]);
-    }, 1000);
+    }, 500);
   }  
   
   checkWinner(){
@@ -71,7 +121,7 @@ class App extends Component {
       if(this.gameState.boardArray[test[i][0]] === this.gameState.boardArray[test[i][1]] ){
         if(this.gameState.boardArray[test[i][1]] === this.gameState.boardArray[test[i][2]] ){
           if(this.gameState.boardArray[test[i][0]] !== ''){
-            return this.gameState.turn;
+            return this.gameState.boardArray[test[i][0]];
           }
         }
       }
@@ -79,27 +129,24 @@ class App extends Component {
     if(this.gameState.count === 9)return 'draw';
     return '';
   } 
-
-  restartGame(){
-
-  }
   
   render() {
     return (
       <div id="game"> 
         <h1>tic_tac_toe Game</h1>
         <div id="board" onClick={ (e)=>this.handleClick(e.target) }>
-            <div className="square" id="0"> </div>
-            <div className="square" id="1"> </div>
-            <div className="square" id="2"> </div>
-            <div className="square" id="3"> </div>
-            <div className="square" id="4"> </div>
-            <div className="square" id="5"> </div>
-            <div className="square" id="6"> </div>
-            <div className="square" id="7"> </div>
-            <div className="square" id="8"> </div>
+            <div className="square" id="0"> {this.gameState.boardArray[0]} </div>
+            <div className="square" id="1"> {this.gameState.boardArray[1]} </div>
+            <div className="square" id="2"> {this.gameState.boardArray[2]} </div>
+            <div className="square" id="3"> {this.gameState.boardArray[3]} </div>
+            <div className="square" id="4"> {this.gameState.boardArray[4]} </div>
+            <div className="square" id="5"> {this.gameState.boardArray[5]} </div> 
+            <div className="square" id="6"> {this.gameState.boardArray[6]} </div>
+            <div className="square" id="7"> {this.gameState.boardArray[7]} </div>
+            <div className="square" id="8"> {this.gameState.boardArray[8]} </div>
         </div>
         <h2> {this.state.result} </h2>
+        <button onClick={()=> this.restartGame()}> Restart Game </button>
       </div> 
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+/*
+localStorage.setItem('gameState',JSON.stringify({
+      turn : 'X' , 
+      turnLocked : false ,
+      boardArray : Array(9).fill('') ,
+      count : 0 ,
+      gameEnded : false 
+    })
+);
+*/
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
